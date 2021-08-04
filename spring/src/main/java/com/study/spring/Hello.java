81f0158dfe27b01379de798e19bf1e8f97d62002
ASUS <ASUS@LAPTOP-5MN2GLU7>
2021-08-04 11:20:49 +0800

    L01_spring_tst_hello

--- spring/src/main/java/com/study/spring/Hello.java
+++ spring/src/main/java/com/study/spring/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("中文");
-		System.out.println("Spring");
+		System.out.println("Spring-00000000000000");
 	}
 }
